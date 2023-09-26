4a4776e3cf6942bf8ceff654f7efde78b22ade4c
kazuha04 <87639688+kazuha04@users.noreply.github.com>
2023-09-26 19:42:05 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ function App() {
   return (
     <>
       <TopNav />
-      <h1>MMAR Website</h1>
-      <h2>The website is in development, please come back later!</h2>
-      <h4>This website will be an OpenSource project in future!!</h4>
+      <div style={{display:'flex',flexWrap:'wrap',justifyContent:'center', alignItems:'center', marginTop:'100px'}}>
+          <div id='title' style={{width:'400px'}}>
+            <h1 style={{fontSize: '30px'}}>MMAR Website</h1>
+            <h2>The website is in development, please come back later!</h2>
+            <h4>This website will be an OpenSource project in future!!</h4>
+          </div>
+        </div>
     </>
   );
 }
